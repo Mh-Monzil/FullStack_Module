f4c7f0dd51fd5c40aa8bafd15a0fc0a42a81ecdb
Mh-Monzil <monzil246@gmail.com>
2025-01-07 12:06:29 +0600

    task-2

--- mongodb/aggreation_practise.ts
+++ mongodb/aggreation_practise.ts
@@ -1,9 +1,18 @@
 // 1___Retrieve the count of individuals who are active (isActive: true) for each gender.
 
-
 db.getCollection("massive-data").aggregate([
     { $match: { isActive: true } },
     { $group: { _id: "$gender", count: { $sum: 1 } } }
 ])
 
 // end 1 
+
+
+// 2___Retrieve the names and email addresses of individuals who are active (`isActive: true`) and have a favorite fruit of "banana."
+
+db.getCollection("massive-data").aggregate([
+    { $match: { isActive: true, favoriteFruit: "banana" } },
+    {$project: {name: 1, email: 1, isActive: 1, favoriteFruit: 1}}
+])
+
+// end 2 
